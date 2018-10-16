1640d2db04d7adfb6ae3aff95615e57da507d81d
estrattonbailey <ericstrattonbailey@gmail.com>
2018-10-16 15:25:39 -0400

    Fix context relative math

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 let listener = null
 let handlers = []
 
-function move ({ clientX: cx, clientY: cy }) {
+function move ({ clientX: dx, clientY: dy }) {
   for (let i = 0; i < handlers.length; i++) {
-    handlers[i](cx, cy)
+    handlers[i](dx, dy)
   }
 }
 
 export default function init(ctx = document.body, targets) {
   if (!listener) listener = document.addEventListener('mousemove', move)
 
-  function handler (cx, cy) {
+  // delta x, y
+  function handler (dx, dy) {
     const {
       left,
       right,
@@ -21,16 +22,16 @@ export default function init(ctx = document.body, targets) {
     } = ctx.getBoundingClientRect()
 
     // are mouse coords inside ctx?
-    if (cx > left && cx < right && cy > top && cy < bottom) {
+    if (dx > left && dx < right && dy > top && dy < bottom) {
       for (let i = 0; i < targets.length; i++) {
         const [ target, speed = 0 ] = [].concat(targets[i])
 
-        const middleX = width / 2
-        const middleY = (height / 2) + top
+        // center of ctx
+        const cx = (width / 2) + left
+        const cy = (height / 2) + top
 
-        // mouse coords relative to ctx
-        const x = ((cx - left) - middleX) * speed
-        const y = ((cy - top) - middleY) * speed
+        const x = (dx - cx) * speed
+        const y = (dy - cy) * speed
 
         requestAnimationFrame(() => {
           target.style.transform = `translate3d(${x}px, ${y}px, 0)`
